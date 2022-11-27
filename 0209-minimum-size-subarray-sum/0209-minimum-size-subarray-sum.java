a5422fc0e3050d664e7f343110501e8a41cd1a08
Amrit Raj <9aamritraj@gmail.com>
2022-11-27 23:54:20 +0530

    Time: 2 ms (84.03%), Space: 58.1 MB (42.87%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -3,24 +3,19 @@ class Solution {
         if (nums == null || target < 0) {
             throw new IllegalArgumentException("Input array is null");
         }
-
         int len = nums.length;
         int start = 0;
         int end = 0;
         int minLen = len + 1;
-
         while (end < len) {
             target -= nums[end];
             end++;
-
             while (target <= 0) {
                 minLen = Math.min(minLen, end - start);
                 target += nums[start];
                 start++;
             }
         }
-
-        return minLen % (len + 1);
-        
+        return minLen % (len + 1); 
     }
 }

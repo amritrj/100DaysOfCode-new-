01cd7604900a9cbfe168a480e19fa1c85236d6e3
Amrit Raj <9aamritraj@gmail.com>
2023-01-23 23:19:57 +0530

    Time: 4 ms (55.79%), Space: 73.1 MB (21.49%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -6,7 +6,7 @@ class Solution {
             count[trust[i][0]]--;
             count[trust[i][1]]++;
         }
-       for(int i = 1;i<count.length;i++){
+       for(int i = 0;i<count.length;i++){
            if(count[i] ==n-1)  return i;
        }
         return -1;

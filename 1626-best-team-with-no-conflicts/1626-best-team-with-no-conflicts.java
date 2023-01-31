04c8875f158cc25035a933aa76012ce10480c054
Amrit Raj <9aamritraj@gmail.com>
2023-01-31 23:52:24 +0530

    Time: 54 ms (58.50%), Space: 42.1 MB (97.28%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -0,0 +1,46 @@
+class Solution {
+    private int dp[];
+    private int max;
+    public int bestTeamScore(int[] scores, int[] ages) {
+        
+        Player[] players = new Player[scores.length];
+        
+    
+        for(int i=0;i<players.length;i++){
+            players[i] = new Player(ages[i],scores[i]);
+        }
+        
+        
+        Arrays.sort(players, (player1,player2)->player1.age==player2.age?player1.score-player2.score:player1.age-player2.age);
+        
+        dp = new int[players.length];
+        Arrays.fill(dp, -1);
+        for(int i=0;i<players.length;i++)
+            max = Math.max(max,dfs(i, players));
+        
+        return max;
+    }
+    
+    public int dfs(int index, Player[] players) {
+        if(dp[index] != -1) return dp[index];
+        
+        dp[index] = players[index].score;
+        for(int i = index+1; i < players.length; i++) {
+            if(players[i].score >= players[index].score) {
+                dp[index] = Math.max(dp[index], dfs(i, players) + players[index].score);
+            }
+        }
+        
+        return dp[index];
+    }
+}
+
+class Player{
+    public int age;
+    public int score;
+        
+    public Player(int age, int score){
+        this.age = age;
+        this.score = score;
+    }
+}

10b8f9831a6800e95a992fc948da45ca98707e34
Amrit Raj <9aamritraj@gmail.com>
2023-08-08 23:47:57 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (100.00%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,34 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int end = 0;
-        int start = nums.length - 1;
-        while (end < start) {
-            int mid = (end + start) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[end] <= nums[mid]) {
-                if (target >= nums[end] && target < nums[mid]) {
-                    start = mid - 1;
+    public int search(int[] arr, int k) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // if mid points to the target
+            if (arr[mid] == k)
+                return mid;
+
+            // if left part is sorted
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    // element exists
+                    high = mid - 1;
+                } else {
+                    // element does not exist
+                    low = mid + 1;
                 }
-                else 
-                {
-                    end = mid + 1;
+            } else { // if right part is sorted
+                if (arr[mid] <= k && k <= arr[high]) {
+                    // element exists
+                    low = mid + 1;
+                } else {
+                    // element does not exist
+                    high = mid - 1;
                 }
-            } 
-            else {
-                if (target > nums[mid] && target <= nums[start]) {
-                    end = mid + 1;
-                } 
-                else {
-                    start = mid - 1;
             }
         }
-    }
-        return nums[end] == target ? end : -1;
+        return -1;
+        
     }
 }
